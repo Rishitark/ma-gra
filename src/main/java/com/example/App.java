c13e63f5172293e8b1803e5c77b9eb2d0e054cf7
Rishitark <rishitarakeshkumar33@gmail.com>
2025-05-11 14:22:37 +0530

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     int a=2;
+     int b=3;
+     int sum=a+b;
+        System.out.println( "sum"+sum);
+    }
+}
